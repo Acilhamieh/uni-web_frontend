2d73db229a0441ebcef89cf65d43c7ab9a91e53c
raghad <raghadinfo12@gmail.com>
2025-06-13 16:08:03 +0300

    finalize the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p className='small-text'>&copy; 2025 Daliluka. Made with 💙 by Raghad & Acil</p>
+        </footer>
+    );
+};
